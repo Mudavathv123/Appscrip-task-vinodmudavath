737a3a03914a6e2ceb90d0c4dd6af9bf179acf1b
Mudavathv123 <mudavathvinod49@gmail.com>
2024-08-11 12:20:29 +0530

    update css

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
 
 import DiscourProductSection from './components/DiscourProductSection/DiscourProductSection'
-import FilterSection from './components/FilterSection/FilterSection'
 import FooterSection from './components/FooterSection/FooterSection'
 import HeaderSection from './components/HeaderSection/HeaderSection'
 import ProductSection from './components/ProductSection/ProductSection'
-
+import './App.css'
 
 function App() {
 
 
   return (
-    <>
-     <HeaderSection />
-     <hr />
-     <DiscourProductSection /> 
-     <ProductSection /> 
-     <FooterSection />
-    </>
+    <div className= "app-container">
+      <HeaderSection />
+      <hr />
+      <DiscourProductSection /> 
+      <ProductSection /> 
+      <FooterSection />
+    </div>
     
   )
 }
